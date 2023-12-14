73b2713f79aaabb74d006d70db8af796f0a507a7
Pere Piñol <pere.pinol@pinja.com>
2023-12-14 15:50:07 +0100

    Several alias improvements

--- add_aliases.sh
+++ add_aliases.sh
@@ -4,11 +4,6 @@ if [ -f "git_aliases.sh" ]; then
     chmod +x git_aliases.sh
     ./git_aliases.sh
 fi
-if [ -f "bash_aliases" ]; then
-    while read line; do
-        if [[ "$line" == \#* ]]; then
-            continue
-        fi
-        echo "$line"
-    done < bash_aliases
+if [ -f "bash_aliases.sh" ]; then
+    cp bash_aliases.sh ~/.bash_aliases
 fi
--- bash_aliases.sh
+++ bash_aliases.sh
@@ -7,36 +7,114 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 alias h='history'
-alias hg='history | grep'
 alias d='docker'
 alias dc='docker-compose'
+alias k='kubectl'
 
-# Runs last instance of the command in the history that starts with the given args
-function hgr() {
-    command=$(history | tail -n +2 | grep "$*" | grep -v hgr | grep -v hg | awk '{$1=""}1' | tail -n 1)
-    echo "$command"
-    $command
+# Prints the given line number from stdin, or a variable if given. Also used in these scripts as a helper
+function line() {
+    [ -z "$1" ] && echo "Usage: line <line_number>" && return 1
+
+    if [ -z "$2" ]; then
+        head -n "$1" | tail -n +"$1"
+    else
+        echo "$2" | head -n "$1" | tail -n +"$1"
+    fi
 }
 
-# Find process IDs by name
+# Finds a command with the given contents in the history
+function hg() {
+    history | tail -n +2 | grep "$*" | grep -v hgr | grep -v hg
+}
+
+# Finds process IDs by name. Like pgrep but with more info
 function process() {
     ps -ef | awk -v search="$1" '
-    /^UID/ { print $0 }
-    {
-        if ($8 ~ search) print $0
-    }'
+        /^UID/ { print $0 }
+        {
+            if ($8 ~ search) print $0
+        }
+    '
 }
 
+# Creates a new directory and cds into it
 function mcd() {
 	mkdir $1
 	cd $1
 }
 
-# Run the last command in the history with `sudo`
+# Runs the last command in the history with `sudo`
 function ensudo() {
     command=$(history | tail -n 2 | head -n 1 | awk '{$1=""}1')
+    history -s "sudo$command"
     echo "sudo$command"
-    sudo $command
+    eval sudo$command
+}
+
+# Makes a beep sound
+function beep() {
+    echo -en '\007'
+}
+
+# Runs a command and beeps when it is done
+function bgrun() {
+    (eval $*; beep) &
+}
+
+# Watches an existing process by name or PID and makes a sound when it finishes
+function bgattach() {
+    [ "$#" -eq 0 ] && echo "No PID or name given" && return 1
+    pid="$*"
+    ps -p $pid &> /dev/null
+    if [ "$?" -gt 0 ]; then
+        pid_names=$(ps -e -o pid,args --no-headers | grep "$*" | head -n -1 | awk '
+            {
+                PIDS[NR]=$1;
+                $1="";
+                NAMES[NR]=$0
+            }
+            END{
+                ORS=" ";
+                for (i = 1; i <= NR; i++) print PIDS[i];
+                ORS="\n";
+                print "";
+                for (i = 1; i <= NR; i++)  print NAMES[i]
+            }
+        ')
+        pids=$(echo "$pid_names" | head -n 1) # All the pids on the same line separated by spaces
+        names=$(echo "$pid_names" | tail -n +2 | sed 's/ //') # Each command and its args on a different line
+        pid="$pids"
+        if [ "$(echo $pid | wc -w)" -gt 1 ]; then
+
+            # Outputs the parameters to the user as options, and returns the index of the chosen one
+            function select_option() {
+                echo "Several options available. Please choose one:" 1>&2
+                argcount=$(echo "$*" | wc -l)
+                for i in $(seq $argcount); do
+                    text=$(line $i "$*")
+                    echo "[$i] $text" 1>&2
+                done
+                read result
+                ! [[ "$result" =~ ^[0-9]+$ ]] || [ "$result" -lt 1 -o "$result" -gt $argcount ] && echo "Invalid value $result" 1>&2 && return 1
+                echo "$((result - 1))"
+            }
+
+            index=$(select_option "$names") || return 1
+            pid_array=($pid)
+            pid=${pid_array[$index]}
+        fi
+    fi
+
+    [ -z "$pid" ] && echo "Process \"$*\" not found" && return 1
+
+    function poll() {
+        while true; do
+            if ! ps -p $pid &> /dev/null; then break; fi
+            sleep 5
+        done
+    }
+
+    bgrun poll
 }
 
 # **************************** git aliases *****************************
@@ -71,7 +149,7 @@ function gsquash() {
             return
     fi
     
-    messages=$(git log master..HEAD --format=%B | awk '/^\\\* .+/{print $0} /^\\\*?[[:alnum:]]+/{print "* "$0}')
+    messages=$(git log master..HEAD --format=%B | awk '/^\* .+/{print $0} /^\*?[[:alnum:]]+/{print "* "$0}')
     
     git reset master --soft
     git add -A
@@ -81,8 +159,8 @@ function gprune() {
     for remote in $(git remote); do
         git remote prune "$remote"
     done
-    for branch in $(git branch | grep -vE "\\\* .*"); do
-        remote=$(git branch --remote | grep "$branch\\\$")
+    for branch in $(git branch | grep -vE "\* .*"); do
+        remote=$(git branch --remote | grep "$branch\$")
         if [ -n "$remote" ]; then continue; fi
         if [ "$1" == "-f" ]; then
             git branch -D "$branch"
